fd533ff941286fd6a00825c33fe1aaf21e0a6e0a
zhaolei <itzhaolei.com>
2021-05-16 00:00:08 +0800

    升级

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "ZLPublicKits",
+    platforms: [
+        .macOS(.v10_11),
+        .iOS(.v9),
+        .tvOS(.v9),
+        .watchOS(.v2)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
