b3cc7f068af31e0a4ea169ddaed9336e94b5bc62
zhaolei <itzhaolei.com>
2021-05-16 19:32:13 +0800

    升级

--- Package.swift
+++ Package.swift
@@ -10,22 +10,22 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "ZLPublicKits", targets: ["ZLPublicKits"]),
+        .library(name: "ZLPublicKits", targets: ["ZLPublicKits"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
 //         .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "MyLibrary"),
+//        .package(path: "MyLibrary"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ZLPublicKits",
-            dependencies: ["MyLibrary"]),
-        .target(
-            name: "OtherFiles",
             dependencies: [])
+//        .target(
+//            name: "OtherFiles",
+//            dependencies: [])
     ],
     swiftLanguageVersions: [
         .v5
